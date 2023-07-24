3a73797cafeb51f6de266acf5158f8f6faf88722
danielJL19 <danieljmnz15@hotmail.com>
2023-07-24 09:51:14 -0400

    feat: se agrego scaffold de jobs

--- db/migrate/20230724132518_add_position_to_users.rb
+++ db/migrate/20230724132518_add_position_to_users.rb
@@ -1,6 +1,6 @@
 class AddPositionToUsers < ActiveRecord::Migration[7.0]
   def change
     add_column :users, :position, :string
-    add_column :users, :roles, :integer
+    add_column :users, :roles, :integer, default:0
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_24_132518) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_24_134327) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "jobs", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_jobs_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,9 +33,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_24_132518) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.string "position"
-    t.integer "roles"
+    t.integer "roles", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "jobs", "users"
 end
